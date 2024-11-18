dfae308635f24a01b4f605d09941e1c8443b0d1b
Юра Челленюк <yurachellenuk@gmail.com>
2024-11-18 16:14:18 +0200

    Використав прості оператори using dbContext в класі NoteDbManager

--- NoteManager/Sevices/Note/NoteDbManager.cs
+++ NoteManager/Sevices/Note/NoteDbManager.cs
@@ -13,85 +13,75 @@ public class NoteDbManager
 
     public void Add(NoteEntity note)
     {
-        using (var dbContext = new NotesDbContext(_path))
+        using var dbContext = new NotesDbContext(_path);
+        try
         {
-            try
-            {
-                var noteDTO = NoteEntityDTO.ToDTO(note);
-                dbContext.Add(noteDTO);
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка додавання нотатки {note.Title} до бази даних {_path}. \n\r {ex.Message}");
-            }
+            var noteDTO = NoteEntityDTO.ToDTO(note);
+            dbContext.Add(noteDTO);
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Помилка додавання нотатки {note.Title} до бази даних {_path}. \n\r {ex.Message}");
         }
     }
 
     public List<NoteEntity> GetAll()
     {
-        using (var dbContext = new NotesDbContext(_path))
+        using var dbContext = new NotesDbContext(_path);
+        try
         {
-            try
-            {
-                var notes = dbContext.GetAll();
-                return notes;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка підключення до бази даних {_path}. \n\r {ex.Message}");
-            }
+            var notes = dbContext.GetAll();
+            return notes;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Помилка підключення до бази даних {_path}. \n\r {ex.Message}");
         }
     }
 
     public void Remove(NoteEntity note)
     {
-        using (var dbContext = new NotesDbContext(_path))
+        using var dbContext = new NotesDbContext(_path);
+        try
         {
-            try
-            {
-                var noteDTO = NoteEntityDTO.ToDTO(note);
-                dbContext.Remove(noteDTO);
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка видалення {note.Title} з бази {_path}. \n\r {ex.Message}");
-            }
+            var noteDTO = NoteEntityDTO.ToDTO(note);
+            dbContext.Remove(noteDTO);
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Помилка видалення {note.Title} з бази {_path}. \n\r {ex.Message}");
         }
     }
 
     public void RemoveByIds(List<Guid> noteIds)
     {
-        using (var dbContext = new NotesDbContext(_path))
+        using var dbContext = new NotesDbContext(_path);
+        try
         {
-            try
-            {
-                dbContext.RemoveNotes(noteIds);
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка видалення нотаток з бази {_path}. \n\r {ex.Message}");
-            }
+            dbContext.RemoveNotes(noteIds);
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Помилка видалення нотаток з бази {_path}. \n\r {ex.Message}");
         }
     }
 
     public void Edit(NoteEntity note, string newContent)
     {
-        using (var dbContext = new NotesDbContext(_path))
+        using var dbContext = new NotesDbContext(_path);
+        try
         {
-            try
-            {
-                var noteDTO = NoteEntityDTO.ToDTO(note);
-                var updatedNote = EditNoteContent(noteDTO, newContent);
-                dbContext.Update(updatedNote);
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка редагування {note.Title} в базі {_path}. \n\r {ex.Message}");
-            }
+            var noteDTO = NoteEntityDTO.ToDTO(note);
+            var updatedNote = EditNoteContent(noteDTO, newContent);
+            dbContext.Update(updatedNote);
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Помилка редагування {note.Title} в базі {_path}. \n\r {ex.Message}");
         }
     }
 
